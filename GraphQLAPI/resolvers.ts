7b198684f5066b6cb6aadb4068978444e41c1022
Anil <786.anil.potlapally@gmail.com>
2022-07-15 17:07:30 +0530

    resolvers.ts : checkAuthenticated , protectedQuery : ADDED

--- GraphQLAPI/resolvers.ts
+++ GraphQLAPI/resolvers.ts
@@ -1,15 +1,45 @@
+import AppError from "../ErrorHandling/AppError";
 import console from "../utils/console";
+
+/////////////////////////////////////////////////////////////////////////////
+// IMPORTANT: NOTE : INFORMATION :  next(err) is called automatically when
+// exception is occured .
+// if errors are thrown the next(err) is called automatically and
+// catched . and we can access them in { extensions : { excepiton : { }  } }
+/////////////////////////////////////////////////////////////////////////////
+
+//#region Portected Query
+
+const checkAuthenticated = (context: any) => {
+  console.log("Check Authenticated");
+  if (context.user) {
+  } else {
+    throw new AppError(
+      "You are not logged in! Please log in to get access.",
+      401
+    );
+  }
+};
+
+function protectedQuery(fn: (_root: any, {}: any, context: any) => any): any {
+  return (_root: any, {}: any, context: any) => {
+    checkAuthenticated(context);
+    return fn(_root, {}, context);
+  };
+}
+
+//#endregion Protected Query End
+
+//#region Query Resolvers .
 
 const resolvers = {
   Query: {
-    greeting: (_root: any, {}, context: any) => {
-      if (context.user) {
-        console.log("\nProtected GraphQL Route\n");
-      }
-
+    greeting: protectedQuery((_root: any, {}: any, context: any) => {
       return "New Hello World";
-    },
+    }),
   },
 };
+
+//#endregion End Query Resolvers .
 
 export default resolvers;
