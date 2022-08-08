f79cc544711d9fad055aa5f56d32dfa993e0e39b
Anil <786.anil.potlapally@gmail.com>
2022-08-08 22:16:58 +0530

    isCurrentApp Added

--- Model/quize.app/quize.models/quize.category.model.ts
+++ Model/quize.app/quize.models/quize.category.model.ts
@@ -10,7 +10,7 @@ import isCurrentApp from "../../../utils/isCurrentApp";
 export default class QuizeCategoryModel implements ModelI<any, any, any> {
   schema: Schema<any> = quizeCategorySchema;
 
-  model: Model<any, any> | null = isCurrentApp()
+  model: Model<any, any> | null = isCurrentApp("QuizeApp")
     ? model<QuizeCategorySI>("quize-categories", this.schema)
     : null;
 }
