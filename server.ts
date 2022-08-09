1e98f5c0281d1ffb5761bf63bd9f6bc53b9aa22c
Anil <786.anil.potlapally@gmail.com>
2022-08-09 06:21:46 +0530

    refactoring

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ import app from "./app";
 import console from "./utils/console";
 import isAllResourcesReady from "./ResourcesVerify/verifyAll";
 import isProductionEnvironment from "./utils/isProductionEnvironment";
+import logger from "./utils/logger";
 
 //////////////////////////////////////////////////////////////////////
 // NOTE :
@@ -37,24 +38,30 @@ if (isAllReady) {
         "<password>",
         getEnv(EnvEnumType.DATABASE_PASSWORD)?.toString() || ""
       ) || "";
-  mongoose
-    .connect(DB)
-    .then((con) => {
-      // console.log(con.connections);
-      console.log("DB connection successfull!\n");
-    })
-    .catch((err) => {
-      console.log("\nDB Connection Error \n", err);
+
+  if (!(DB.indexOf("_MODE_") > -1)) {
+    console.log("_MODE_ doesnot exists in the config.env");
+    logger.resourceNotFoundError("_MODE_ doesnot exists in the config.env");
+  } else {
+    mongoose
+      .connect(DB)
+      .then((con) => {
+        // console.log(con.connections);
+        console.log("DB connection successfull!\n");
+      })
+      .catch((err) => {
+        console.log("\nDB Connection Error \n", err);
+      });
+    //#endregion
+
+    //#region  listen
+
+    app.listen(getEnv(EnvEnumType.PORT), () => {
+      console.log("\n\n\n******** NODE SERVER STARTED *************\n\n");
+      console.log("Listening on port 80");
+      console.log("GrapQL Url :", "http://localhost/graphql");
+      console.log("isProduction", isProductionEnvironment());
     });
-  //#endregion
-
-  //#region  listen
-
-  app.listen(getEnv(EnvEnumType.PORT), () => {
-    console.log("\n\n\n******** NODE SERVER STARTED *************\n\n");
-    console.log("Listening on port 80");
-    console.log("GrapQL Url :", "http://localhost/graphql");
-    console.log("isProduction", isProductionEnvironment());
-  });
-  //#endregion
+    //#endregion
+  }
 }
