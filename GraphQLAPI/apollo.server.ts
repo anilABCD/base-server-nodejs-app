f8226dc3edf1bd42ef52699f94fab2bf1995169e
Anil <786.anil.potlapally@gmail.com>
2022-10-03 09:04:52 +0530

    working

--- GraphQLAPI/apollo.server.ts
+++ GraphQLAPI/apollo.server.ts
@@ -8,6 +8,7 @@ import console from "../utils/console.js";
 import AppError from "../ErrorHandling/AppError.js";
 import isProductionEnvironment from "../utils/isProductionEnvironment.js";
 import getEnv, { EnvEnumType } from "../env/getEnv.js";
+import File, { FileParams } from "../utils/File.js";
 
 async function startApolloServer() {
   // const typeDefs = await readFile(
@@ -19,10 +20,28 @@ async function startApolloServer() {
 
   const pathForCurrentApp = CURRENT_APP?.replace(/-/g, ".");
 
-  let currentProjectTypeDefs = await readFile(
-    `${__dirname}/${pathForCurrentApp}/schema.graphql`,
-    "utf-8"
+  // let currentProjectTypeDefs = await readFile(
+  //   `${__dirname}/${pathForCurrentApp}/schema.graphql`,
+  //   "utf-8"
+  // );
+
+  const fileParams: FileParams = {
+    namesOf: "file",
+  };
+
+  let fileNamesTypeDefs = File.getDirectoryOrFileNamesSync(
+    [`${__dirname}/${pathForCurrentApp}`],
+    fileParams,
+    ["graphql"]
   );
+
+  let currentProjectTypeDefs = File.getFilesDataSync(
+    fileNamesTypeDefs,
+    ["graphql"],
+    []
+  );
+
+  // console.log(fileNamesTypeDefs, "typedefs filenames");
 
   let currentProjectResolvers =
     require(`${__dirname}/${pathForCurrentApp}/resolvers.js`).default;
